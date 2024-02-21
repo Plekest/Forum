9dfe83a67da742ef8a3dae8fd0d0b257daa864c3
Pedro <phtf.pedro12@gmail.com>
2024-02-21 00:11:59 -0300

    aula 16

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -7,14 +7,20 @@
 @stop
 
 @section('content')
+
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            {{ $error }}
+        @endforeach
+    @endif
     <form action="{{ route('supports.store') }}" method="POST">
         {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
         @csrf()
         <div class="form-group">
-            <input type="text" placeholder="Assunto" name="subject">
+            <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}">
         </div>
         <div class="form-group">
-            <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+            <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
         </div>
         <button class="btn btn-primary" type="submit">Enviar</button>
     </form>
